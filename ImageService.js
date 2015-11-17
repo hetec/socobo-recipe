d6f9318524bd9f261a68460aa7b2aad8c39c9377
hetec <hebner.tec@gmail.com>
2015-11-17 20:03:58 +0100

    #ba-79 Transform the chosen image to a string

--- ImageService.js
+++ ImageService.js
@@ -1,23 +1,24 @@
-(function ImageService(imageCooserId){
-  var imageChooser = document.querySelector('#' + imageCooserId);
-
+function ImageService(imageCooserId){
+  var imageChooser = document.querySelector(imageCooserId);
+  alert(imageChooser);
   var data;
-
-  var encode = function () {
+  this.encode = function () {
     var file = imageChooser.files[0];
+    alert("file: " + file.name);
     var reader = new FileReader();
     reader.onload = function(e){
       data = e.target.result;
+      alert("finished");
+    }
+    reader.onloadstart = function(e){
+      alert("start");
     }
     reader.readAsDataURL(file);
   };
 
   this.getImageAsString = function(){
-    alert(data);
+    alert("Data: " + data);
   };
 
-  imageChooser.addEventListener("change", function () {
-    encode();
-  });
-})();
+}
 
