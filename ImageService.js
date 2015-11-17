8fa58ed81bfde4f4df6cf7c59a02593669b349dc
hetec <hebner.tec@gmail.com>
2015-11-17 21:14:23 +0100

    #ba-79 Display uploaded image in the add view

--- ImageService.js
+++ ImageService.js
@@ -1,12 +1,13 @@
 function ImageService(imageCooserId){
   var imageChooser = document.querySelector(imageCooserId);
   var data;
-  this.encode = function () {
+  this.encode = function (imageId) {
     var file = imageChooser.files[0];
     var reader = new FileReader();
     reader.onload = function(e){
       data = e.target.result;
       alert("finished");
+      build(imageId, data);
     }
     reader.readAsDataURL(file);
   };
@@ -14,6 +15,14 @@ function ImageService(imageCooserId){
   this.getImageAsString = function(){
     return data;
   };
+
+  var build = function (id, data) {
+    var img = document.createElement('img');
+    img.style.height = '280px';
+    img.style.width = '280px';
+    img.src = data;
+    id.innerHTML = img.outerHTML;
+  }
 
 }
 
