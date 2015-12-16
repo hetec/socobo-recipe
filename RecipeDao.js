7fcb253780e774d57a9c8de321323f45003b1b74
Sebastian Kloppe <feedbackcalcest@gmail.com>
2015-12-16 22:01:21 +0100

    several changes
    - add test db
    - rewrite RecipeDao.js to Singleton
    - add Promises all the way
    - little changes here and there

--- RecipeDao.js
+++ RecipeDao.js
@@ -1,162 +1,200 @@
-function RecipeDao(component, userId) {
-    var that = component;
-    var id = userId;
-    var userUrl = getUserUrl();
+var RecipeDao = (function(){
+  /**
+   * Instance stores a reference to the Singleton
+   */
+  var instance;
 
-    function getUserUrl() {
-      return that.url + "recipes/" + id;
+  function init(user) {
+    /**
+     *
+     */
+    var _baseUrl = user.firebaseUrl;
+    var _id = user.userId;
+    var _userUrl = _getUserUrl();
+
+    /**
+     *
+     * @returns {string}
+     * @private
+     */
+    function _getUserUrl() {
+      return _baseUrl + "recipes/" + _id;
     }
 
-    this.add = function (obj) {
-      var dataRef = new Firebase(userUrl);
-      dataRef.push(obj, function (error) {
-        if (error) {
-          notify("Sorry a technical error occured while saving your recipe :(");
-        } else {
-          notify("Element successfully created!");
-        }
-      });
-    };
-
-    this.findImage = function(recipe){
-      var dbUrl = (recipe.ref) + "/image";
-      var image = undefined;
-      var dataRef = new Firebase(dbUrl);
-      dataRef.once("value", function (snapshot) {
-        image = snapshot.val();
-      }, function(error){
-        notify("Getting image from the database failed");
-      });
-      return image;
-    }
-
-    this.update = function (obj) {
-      var reference = "" + obj.ref + "";
-      var newObj = {};
-      for (var e in obj) {
-        if (e != "ref" && e != "info") {
-          if(checkProperty(obj, e)){
-            newObj[e] = obj[e];
-          }
-        }
-      }
-      var dataRef = new Firebase(reference);
-      dataRef.set(newObj, function (error) {
-        if (error) {
-          notify("Sorry a technical error occured while updating your recipe :(");
-        } else {
-          notify("Element successfully updated!");
-        }
-      });
-      that.recipes = [];
-    };
-
-    this.getAll = function(){
-      var dataRef = new Firebase(userUrl);
-      dataRef.once("value", function (snapshot) {
-        var val = snapshot.val();
-        for(e in val){
-          var recipe = {};
-          var source;
-          if(checkProperty(val, e)){
-            source =  val[e];
-            recipe.ref = snapshot.ref() + "/" + e;
-            recipe.desc = source.desc;
-            recipe.info = source.info;
-            recipe.ingredients = [];
-            recipe.steps = [];
-            recipe.text = source.text;
-            fillArrayProperty(source, recipe, "ingredients");
-            fillArrayProperty(source, recipe, "steps");
-          }else {
-            recipe.ref = snapshot.ref() + "/" + e;
-            recipe.desc = "no titel";
-            recipe.info = "-";
-            recipe.ingredients = [];
-            recipe.steps = [];
-            recipe.text = "no description";
-          }
-          that.push("recipes", recipe);
-        }
-      }, function(err){
-        notify("Sorry a technical error occured while fetching your recipes :(");
-      });
-    };
-
-    this.getAndUpdate = function () {
-      var dataRef = new Firebase(userUrl);
-      dataRef.on("child_added", function (snapshot) {
-        new Promise(function (resolve, reject) {
-          resolve(snapshot.val());
-        }).then(function (val) {
-          var recipe = {};
-          recipe.ref = snapshot.ref();
-          recipe.desc = val.desc;
-          recipe.info = val.info;
-          recipe.ingredients = [];
-          recipe.steps = [];
-          recipe.text = val.text;
-          fillArrayProperty(val, recipe, "ingredients");
-          fillArrayProperty(val, recipe, "steps");
-          that.push('recipes', recipe);
-        }).catch(function (err) {
-          notify(err);
-        });
-      }, function (err) {
-        notify(err);
-      });
-    };
-
-    this.remove = function (obj, succ, err) {
-      if (typeof succ === 'undefined') {
-        succ = function () {
-          notify("Element successfully removed!");
-        }
-      }
-      if (typeof err === 'undefined') {
-        err = function () {
-          notify("Sorry a technical error occured while deleting your recipe :(");
-        }
-      }
-      if (typeof succ !== 'function') {
-        throw "The callback for success in removeRecipe() is not a function";
-      }
-      if (typeof err !== 'function') {
-        throw "The callback for errors in removeRecipe() is not a function";
-      }
-      var dataRef = new Firebase("" + obj.ref);
-      dataRef.remove(function (error) {
-        if (error) {
-          err();
-        } else {
-          succ();
-        }
-      });
-    };
-
-    function checkProperty(obj, propName){
+    /**
+     *
+     * @param obj
+     * @param propName
+     * @returns {boolean}
+     * @private
+     */
+    function _checkProperty(obj, propName){
       return obj.hasOwnProperty(propName);
     }
 
-    function fillArrayProperty(source, dest, arrProp){
-      if(checkProperty(dest, arrProp)){
+    /**
+     *
+     * @param source
+     * @param dest
+     * @param arrProp
+     * @private
+     */
+    function _fillArrayProperty(source, dest, arrProp){
+      if(_checkProperty(dest, arrProp)){
         for (var j in source[arrProp]) {
-          if(checkProperty(source[arrProp], j)){
+          if(_checkProperty(source[arrProp], j)){
             (dest[arrProp]).push(source[arrProp][j]);
           }
         }
       }
     }
 
-    function notify(msg){
-      that.$.errorNotification.text=msg;
-      that.$.errorNotification.show();
+    /**
+     *
+     * @param obj
+     * @returns {Promise}
+     */
+    function add(obj) {
+      return new Promise(function(resolve, reject) {
+        var dataRef = new Firebase(_userUrl);
+        dataRef.push(obj, function (error) {
+          if (error) {
+            reject({value: "Sorry a technical error occured while saving your recipe :("});
+          } else {
+            resolve({value: "Element successfully created!"});
+          }
+        });
+      });
     }
-}
 
+    /**
+     *
+     * @param obj
+     * @returns {Promise}
+     */
+    function update(obj) {
+      //
+      var reference = obj.ref.toString();
+      //
+      var newObj = {};
+      for (var e in obj) {
+        if (e != "ref" && e != "info") {
+          if(_checkProperty(obj, e)){
+            newObj[e] = obj[e];
+          }
+        }
+      }
+      //
+      return new Promise(function(resolve, reject) {
+        var dataRef = new Firebase(reference);
+        dataRef.set(newObj, function (error) {
+          if (error) {
+            reject({value: "Sorry a technical error occured while updating your recipe :("});
+          } else {
+            resolve({value: "Element successfully updated!"});
+          }
+        });
+      });
+    }
 
+    /**
+     *
+     * @param obj
+     * @returns {Promise}
+     */
+    function remove(obj) {
+      return new Promise(function(resolve, reject) {
+        var dataRef = new Firebase(obj.ref.toString());
+        dataRef.remove(function (error) {
+          if (error) {
+            reject({value: "Sorry a technical error occured while deleting your recipe :("});
+          } else {
+            resolve({value: "Element successfully removed!"});
+          }
+        });
+      });
+    }
 
+    /**
+     *
+     * @returns {Promise}
+     */
+    function getAll(){
+      return new Promise(function(resolve, reject) {
+        var recipes = [];
+        var dataRef = new Firebase(_userUrl);
+        dataRef.once("value", function (snapshot) {
+          var val = snapshot.val();
+          for(var e in val){
+            var recipe = {};
+            var source;
+            if (_checkProperty(val, e)) {
+              source =  val[e];
+              recipe.ref = snapshot.ref() + "/" + e;
+              recipe.desc = source.desc;
+              recipe.info = source.info;
+              recipe.ingredients = [];
+              recipe.steps = [];
+              recipe.text = source.text;
+              _fillArrayProperty(source, recipe, "ingredients");
+              _fillArrayProperty(source, recipe, "steps");
+            } else {
+              recipe.ref = snapshot.ref() + "/" + e;
+              recipe.desc = "no titel";
+              recipe.info = "-";
+              recipe.ingredients = [];
+              recipe.steps = [];
+              recipe.text = "no description";
+            }
+            recipes.push(recipe);
+          }
+          resolve({value: recipes});
+        }, function(err){
+          reject({value: "Sorry a technical error occured while fetching your recipes :("});
+        });
+      });
+    }
 
+    /**
+     *
+     * @param recipe
+     * @returns {Promise}
+     */
+    function findImage(recipe){
+      return new Promise(function(resolve, reject) {
+        var dbUrl = recipe.ref.toString() + "/image";
+        var dataRef = new Firebase(dbUrl);
+        dataRef.once("value", function (snapshot) {
+          resolve({value: snapshot.val()});
+        }, function(error){
+          reject({value: "Getting image from the database failed"});
+        });
+      });
+    }
 
+    /**
+     *
+     */
+    return {
+      add    : add,
+      update : update,
+      remove : remove,
+      getAll : getAll,
+      findImage : findImage
+    };
+  }
 
-
+  return {
+    /**
+     * Get the Singleton instance if one exist or create one if it doesn't
+     * @param user
+     * @returns {Object}
+     */
+    getInstance: function(user) {
+      if (!instance) {
+        instance = init(user);
+      }
+      return instance;
+    }
+  };
+})();
